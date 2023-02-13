5f9003b7bb9d362ac7061b1656b0f96a91127403
jon-cron <jonathanacm95@gmail.com>
2023-02-13 15:22:21 -0700

    push

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+// NOTE before starting to type run the following commands in powershell: 1. "yarn add react-navigation" 2. "npx expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view -- --legacy-peer-deps" 3. "yarn add react-navigation-stack @react-native-community/masked-view" 4. go to Babel.config.js
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 export default function App() {
   return (
     <View style={styles.container}>
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,7 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // NOTE step 4. copy and paste the plugin line of code to your babel
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
